0160a82db556db1973068fd370fa291dd39cf328
mulsewm <mulsewm@protonmail.com>
2023-11-10 19:05:48 +0300

    footer copyright date to Date type

--- Front/app/components/Footer/Footer.tsx
+++ Front/app/components/Footer/Footer.tsx
@@ -1,55 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 
-interface ProductType {
-    id: number;
-    section: string;
-    link: string[];
-}
 
-interface socialLinks {
-    imgSrc: string;
-    link: string;
-    width: number;
-}
 
-const socialLinks: socialLinks[] = [
-    {
-        imgSrc: '/assets/footer/facebook.svg',
-        link: 'www.facebook.com',
-        width: 10
-    },
-    {
-        imgSrc: '/assets/footer/insta.svg',
-        link: 'www.instagram.com',
-        width: 14
-    },
-    {
-        imgSrc: '/assets/footer/twitter.svg',
-        link: 'www.twitter.com',
-        width: 14
-    },
 
-]
 
-const products: ProductType[] = [
-    {
-        id: 1,
-        section: "Company",
-        link: ['About', 'Careers', 'Mobile', 'Blog', 'How we work?'],
-    },
-    {
-        id: 2,
-        section: "Contact",
-        link: ['Help/FAQ', 'Press', 'Affiliates', 'Hotel owners', 'Partners']
-    }
-    ,
-    {
-        id: 3,
-        section: "More",
-        link: ['Airline fees', 'Airlines', 'Low fare tips', 'Badges &', 'Certificates']
-    }
-]
 
 const footer = () => {
     return (
@@ -60,7 +15,9 @@ const footer = () => {
             {/* All Rights Reserved */}
 
             <div className='py-10 md:flex items-center justify-between border-t border-t-gray-blue'>
-                <h4 className='text-dark-red opacity-75 text-sm text-center md:text-start font-normal'>@2023 UMS.All rights reserved</h4>
+                <h4 className='text-dark-red opacity-75 text-sm text-center md:text-start font-normal'> {`© ${new Date().getFullYear()}, Made with `}
+
+          ❤️ Mulsew M</h4>
                 <div className="flex gap-5 mt-5 md:mt-0 justify-center md:justify-start">
                     <h4 className='text-dark-red opacity-75 text-sm font-normal'><Link href="/" target="_blank">Privacy policy</Link></h4>
                     <div className="h-5 bg-dark-red opacity-25 w-0.5"></div>
